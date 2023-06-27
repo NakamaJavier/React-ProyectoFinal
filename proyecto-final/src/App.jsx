f059ab711510c5d15ca010d8b00db9a2591c02fb
Javier Nakama <nakamajavier@hotmail.com>
2023-06-27 15:14:27 -0300

    Se configuro un poco el firebase y se coloco una funcion para parsear un json y subir a partir de este la base de datos de los productos a vender al firebase

--- proyecto-final/src/App.jsx
+++ proyecto-final/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import cargarDatos from "./firebase/firebaseConfig"
+////////////////////////////////////////////////////////////////////////////////////////
+// fetch('../public/productos_bd.json')
+//     .then(response => response.json())
+//         .then(productosParseados=> {
+//             const productos =productosParseados
+//             console.log("Cargo por el json productos_bd:",productos);
+//             cargarDatos(productos)
+//         })
 
+////////////////////////////////////////////////////////////////////////////////7
 function App() {
   console.log(import.meta.env);
   return (
     <div className= "body">
-      
+      <h1>Hola</h1>
     </div>
   )
 }
--- proyecto-final/src/firebase/firebaseConfig.js
+++ proyecto-final/src/firebase/firebaseConfig.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore,collection,addDoc,setDoc,doc} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +14,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+
+
+
+const cargarDatos = async (items)=> {
+    const docRef = await setDoc(doc(db,"shoes","productos"),{
+        items
+    })
+}
+
+export default cargarDatos;
