a489bfcea3c44c55fec18e156c29178392b09a4f
Javier Nakama <nakamajavier@hotmail.com>
2023-07-05 15:15:06 -0300

    adf

--- proyecto-final/vite.config.js
+++ proyecto-final/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-ProyectoFinal/"
+  base: "/React-ProyectoFinal/",
+  build: {
+    base: '/React-ProyectoFinal/',
+  }
 })
